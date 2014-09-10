d7b93662debc80afc20d09a4885b3b0f5427d3e0
Nick Spinazze <user.email>
2014-09-10 17:01:28 -0500

    Initial Commit

---
+++ UITableViewControllerChallengeSolution/TCViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCViewController.h
+//  UITableViewControllerChallengeSolution
+//
+//  Created by Nicholas Spinazze on 9/10/14.
+//  Copyright (c) 2014 Nicholas Spinazze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCViewController : UIViewController
+
+@end
---
+++ UITableViewControllerChallengeSolution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewControllerChallengeSolution
+//
+//  Created by Nicholas Spinazze on 9/10/14.
+//  Copyright (c) 2014 Nicholas Spinazze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+    }
+}
